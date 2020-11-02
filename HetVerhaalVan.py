4ff85c2ae30610e832c769780ef76e3d226b1b77
Jayden Macbean <61696738+JaydenMacbean@users.noreply.github.com>
2020-11-02 12:01:39 +0100

    Add files via upload
    This sucks

---
+++ HetVerhaalVan.py
@@ -0,0 +1,29 @@
+print("Je wordt wakker in een truck. Het is krap, je zit opgescheept met drie andere mensen... zonder enig idee waar je bent.")
+print("Jouw opties zijn: ")
+print("A. Door blijven slapen")
+print("B. Vragen aan je mede mensen wat er gebeurt")
+print("C. Kijk naar buiten")
+AntwoordA1 = "A"
+AntwoordB1 = "B"
+AntwoordC1 = "C"
+inputVraag1 = input("TYP IN A, B OF C: ")
+if(inputVraag1 == AntwoordA1):
+    print("In je dromen hoor je gegil, geschreeuw en het geluid van afvurende wapens... Waarom weet je niet precies.")
+    print("In de achtergrond verschijnt er een man, de details van het figuur's gezicht zijn moeilijk te verwerken.")
+    print("Wat doe je?")
+    print("A. Ren weg")
+    print("B. Probeer dichterbij te komen.")
+    AntwoordA2 = "A"
+    AntwoordB2 = "B"
+    AntwoordC2 = "C"
+    inputVraag2 = input("TYP IN A, B OF C: ")
+    if(inputVraag2 == AntwoordA2):
+        print("Uit angst ren je weg")
+        print("In de achtergrond hoor je geluiden... ren weg... ga hier vandaan ")
+    
+if(inputVraag1 == AntwoordB1): 
+    print("Blablabla Placeholder")
+if(inputVraag1 == AntwoordC1):
+    print("Blablabla Placeholder") 
+
+    
